9feccf8806df564e033a26b9cc5ea82240253166
kathrin <kathrin.koenig@htl-salzburg.ac.at>
2019-12-10 16:41:53 +0100

    refactoring: added comments

--- Assets/MyGame/Scripts/ComputeNumbers.cs
+++ Assets/MyGame/Scripts/ComputeNumbers.cs
@@ -3,10 +3,12 @@ using UnityEngine.UI;
 
 public class ComputeNumbers : MonoBehaviour
 {
+    // init variables
     public InputField ipVarA;
     public InputField ipVarB;
     public Text result;
 
+    // show result in text-box
     public void SetResult()
     {
         float a = float.Parse(ipVarA.text);
@@ -14,6 +16,7 @@ public class ComputeNumbers : MonoBehaviour
         result.text = SubtractNumbers(a, b).ToString();
     }
 
+    // substract numbers and return result value
     private float SubtractNumbers(float variableA, float variableB)
     {
         float result = variableA - variableB;
